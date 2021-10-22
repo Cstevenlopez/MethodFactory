771716b89c7fdcaceb3b5704ec90b66bbc28c034
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-22 14:08:32 -0600

    Finalizacion de la clase Figuras

--- src/main/java/org/steven/Figuras.java
+++ src/main/java/org/steven/Figuras.java
@@ -1,4 +1,14 @@
 package org.steven;
 
-public class Figuras {
+import java.awt.*;
+public abstract class Figuras {
+
+    int lado = 0;
+    public Figuras(int lado) {
+        this.lado = lado;
+    }
+    public abstract double getArea();
+    public abstract void dibujar(Graphics g, int x, int y);
+
 }
+
