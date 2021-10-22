7b5ed583cf8b4479d722a4305ad9227ac6e82dcd
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-22 11:29:04 -0600

    Finalizacion de la clase Circulo

--- src/main/java/org/steven/Circulo.java
+++ src/main/java/org/steven/Circulo.java
@@ -1,4 +1,19 @@
 package org.steven;
 
-public class Circulo {
+import java.awt.*;
+
+public class Circulo extends Figura {
+
+    public Circulo(int lado){
+        super(lado);
+    }
+
+    public double getArea(){
+        return (Math.PI * lado * lado);
+    }
+
+    public void dibujar(Graphics g,int x, int y){
+        g.setColor(Color.BLACK);
+        g.drawArc(x, y, lado, lado, 0, 360);
+    }
 }
